7f2748904169ada8cfd593b8511e784b156114c1
Yqzi <y.qazi12@gmail.com>
2023-07-14 17:56:32 -0700

    ????

--- lib/screen.dart
+++ lib/screen.dart
@@ -1,86 +1,105 @@
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:image_picker/image_picker.dart';
 
-class NextScreen extends StatelessWidget {
+class NextScreen extends StatefulWidget {
   const NextScreen({super.key});
 
-  void openGallery() async {
-    ImagePicker picker = ImagePicker();
-    XFile? image = await picker.pickImage(source: ImageSource.gallery);
+  @override
+  State<NextScreen> createState() => _NextScreenState();
+}
+
+class _NextScreenState extends State<NextScreen> {
+  // void openGallery() async {
+  ImagePicker picker = ImagePicker();
+  Uint8List? imageBytes;
+
+  void imagePicker() async {
+    XFile? _image = await picker.pickImage(source: ImageSource.gallery);
+    imageBytes = await _image?.readAsBytes();
+    setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Column(
-        children: [
-          Container(
-            color: const Color.fromARGB(255, 238, 236, 236),
-            height: MediaQuery.of(context).size.height / 3 - 56,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    return Column(
+      children: [
+        Container(
+          color: const Color.fromARGB(255, 238, 236, 236),
+          height: MediaQuery.of(context).size.height / 3 - 56,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  TextButton(onPressed: null, child: Text('cancel')),
+                  Text('New Contact'),
+                  TextButton(onPressed: null, child: Text('done'))
+                ],
+              ),
+              ClipRRect(
+                borderRadius: BorderRadius.circular(50),
+                child: MaterialButton(
+                  onPressed: imagePicker,
+                  child: SizedBox(
+                    width: 100,
+                    height: 100,
+                    child: imageBytes == null
+                        ? const Icon(
+                            Icons.account_circle,
+                            size: 100,
+                          )
+                        : Image.memory(
+                            imageBytes!,
+                            fit: BoxFit.cover,
+                          ),
+                  ),
+                ),
+              ),
+              TextButton(onPressed: imagePicker, child: Text('Add Photo')),
+            ],
+          ),
+        ),
+        Container(
+          color: const Color.fromARGB(255, 238, 236, 236),
+          height: MediaQuery.of(context).size.height * 2 / 3,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Container(
+                color: Colors.white,
+                child: const Column(
                   children: [
-                    TextButton(onPressed: null, child: Text('cancel')),
-                    Text('New Contact'),
-                    TextButton(onPressed: null, child: Text('done'))
+                    TextField(
+                      textAlign: TextAlign.left,
+                      decoration: InputDecoration(
+                          hintText: 'First Name',
+                          contentPadding: EdgeInsets.all(15.0)),
+                    ),
+                    TextField(
+                      textAlign: TextAlign.left,
+                      decoration: InputDecoration(
+                          hintText: 'Last Name',
+                          contentPadding: EdgeInsets.all(15.0)),
+                    ),
                   ],
                 ),
-                SizedBox(
-                  width: double.infinity,
-                  child: IconButton(
-                      onPressed: openGallery,
-                      icon: const Icon(
-                        Icons.account_circle,
-                        size: 100,
-                      )),
+              ),
+              Container(
+                color: Colors.white,
+                child: const TextField(
+                  textAlign: TextAlign.left,
+                  decoration: InputDecoration(
+                      hintText: 'add phone',
+                      contentPadding: EdgeInsets.all(15.0)),
                 ),
-                TextButton(onPressed: openGallery, child: Text('Add Photo')),
-              ],
-            ),
+              ),
+            ],
           ),
-          Container(
-            color: const Color.fromARGB(255, 238, 236, 236),
-            height: MediaQuery.of(context).size.height * 2 / 3,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Container(
-                  color: Colors.white,
-                  child: const Column(
-                    children: [
-                      TextField(
-                        textAlign: TextAlign.left,
-                        decoration: InputDecoration(
-                            hintText: 'First Name',
-                            contentPadding: EdgeInsets.all(15.0)),
-                      ),
-                      TextField(
-                        textAlign: TextAlign.left,
-                        decoration: InputDecoration(
-                            hintText: 'Last Name',
-                            contentPadding: EdgeInsets.all(15.0)),
-                      ),
-                    ],
-                  ),
-                ),
-                Container(
-                  color: Colors.white,
-                  child: const TextField(
-                    textAlign: TextAlign.left,
-                    decoration: InputDecoration(
-                        hintText: 'add phone',
-                        contentPadding: EdgeInsets.all(15.0)),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
